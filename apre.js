7f0d2f2efe85ca90231a0e50d9ecb9ed16786746
Matheus S. <matheus.elaine.nana@gmail.com>
2026-05-22 21:36:23 +0000

    final

---
+++ apre.js
@@ -0,0 +1,27 @@
+let slideAtual = 0;
+
+const slides = document.querySelectorAll(".slide");
+
+function mostrarSlide(index){
+
+    slides.forEach(slide => {
+        slide.classList.remove("active");
+    });
+
+    slides[index].classList.add("active");
+}
+
+function trocarSlide(direcao){
+
+    slideAtual += direcao;
+
+    if(slideAtual >= slides.length){
+        slideAtual = 0;
+    }
+
+    if(slideAtual < 0){
+        slideAtual = slides.length - 1;
+    }
+
+    mostrarSlide(slideAtual);
+}
